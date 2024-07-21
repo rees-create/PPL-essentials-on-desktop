8f264050c1a9ae2601e4abfc10d14d4841e06b7f
Rees Amarteifio <reesamarteifio@gmail.com>
2024-07-21 00:09:31 -0500

    added some picture formats to gitignore, added blender.py (paper plane land terrain blending algorithm testing)

---
+++ blender.py
@@ -0,0 +1,90 @@
+import math
+import matplotlib.pyplot as plt
+from scipy.interpolate import interp2d
+import numpy as np
+
+
+#ya we codin' blender version paperplanedev up here... nah just the blending algorithm
+class Vector2:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+    def veclist(self, tup_list):
+        vec_list = []
+        for tup in tup_list:
+            vec_list.append(Vector2(tup[0], tup[1]))
+        return vec_list
+        
+    def __repr__(self):
+        return str((self.x, self.y))
+    
+    def __add__(self, other):
+        return (self.x + other.x, self.y + other.y)
+    
+    def __sub__(self, other):
+        return (self.x - other.x, self.y - other.y)
+    
+    def magnitude(self):
+        pass
+
+def findAdjacents(index, diagonals = True):
+    #vertical, diagonal, horizontal, negative diagonal
+    directions = Vector2.veclist([(1,0), (1,-1), (0,1), (1,1)]) if diagonals else Vector2.veclist([(1,0), (0,1)])
+    adjacent_indices = [(0,0) for i in range(2 * len(directions))]
+    
+    for i in range(len(directions)):
+        idx_pos = (i, i+len(directions)) if i // 2 % 2 == 0 else (i+len(directions), i)
+        adjacent_indices[idx_pos[0]] = index - directions[i]
+        adjacent_indices[idx_pos[1]] = index + directions[i]
+    return adjacent_indices
+
+def DirectionalXY(x, y, numDirections, waveIndex):
+    axis = (waveIndex * math.pi) / numDirections
+    xComponent = x * math.sin(axis)
+    yComponent = y * math.cos(axis)
+    return xComponent + yComponent
+
+def ppl_heightmap(dimensions, nOctaves, wavelength, persistence, lacunarity, numWaves):
+    heightmap = [[0 for y in range(len(dimensions.y))] for x in range(len(dimensions.x))]
+
+    for z in range(len(dimensions.y)):
+        for x in range(len(dimensions.x)):
+            pointValue = 0
+            for waveIndex in range(1, numWaves + 1):
+                ensembleValue = 0
+                for _octave in range (1, nOctaves + 1):
+                    alpha = persistence ** _octave
+                    omega = (1/wavelength) * (lacunarity ** _octave)
+            
+                    directionalTheta = DirectionalXY(x, z, numWaves, waveIndex)
+                    directionalSizeTheta = dimensions.x + dimensions.y
+                    
+                    if directionalSizeTheta == 0:
+                        directionalSizeTheta = 1
+                    
+                    parameter = omega * directionalTheta / directionalSizeTheta
+                    wave = math.sin(parameter)
+                    octave = alpha * wave
+                    ensembleValue += octave
+                    
+                pointValue += ensembleValue
+                
+            heightmap[z][x] = pointValue
+            
+    return heightmap #let's not normalize for now; it's definitely something to consider though
+
+def weightInterpolation(heightmap, spline, res, start_percent, direction):
+    #first create filler linspace.. no! avoid this step! The spline should run through the already given heightmap
+    #instead, first create a coordinate grid variable and interpolation point grid variable
+    #z in interp2d should be the heights in the heightmap, x, and y are the coordinates
+    pass
+    
+    #x_interp = np.linspace(np.min(grid, axis = 0), np.max(grid, axis = 0), len(grid) * res)
+
+    
+
+
+
+
+                    
